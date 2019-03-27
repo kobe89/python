6e6dacaac5a72902f125fb59245ce2370de42cdd
kobe89 <48972037+kobe89@users.noreply.github.com>
2019-03-27 09:47:40 +0800

    Add files via upload

---
+++ 23人同屋生日同日概率.py
@@ -0,0 +1,33 @@
+from random import randint
+
+def randbirth():
+    mon=randint(1,12)
+    if mon in[1,3,5,7,8,10,12]:
+        day=randint(1,31)
+    elif mon ==2:
+        day=randint(1,28)
+    else:
+        day=randint(1,30)
+    return mon*100+day
+ 
+def main():
+    ls=[]
+    for i in range(10):
+        ls.append(randbirth())
+    if len(ls) == len(set(ls)):
+        return 0
+    else:
+        return 1
+
+try:
+    poss=0
+    n=eval(input("请输入样本数量："))
+    for i in range(n):
+        if main() == 1:
+            poss+=1
+    if (poss/n) >= 0.5:
+        print("当房间内的人数为 10 人时，他们生日几率超过了 50%,是{}%".format(poss*100/n))
+    else:
+        print("当房间内的人数为 10 人时，他们生日几率没有一半,是{}%".format(poss*100/n))
+except:
+    print("输入有误") 
